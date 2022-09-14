541b48d0bc70f51da8cc7d3e9d0cee2266f754a4
Everest Wonder <mounteverestwonder@gmail.com>
2022-09-13 22:28:13 -0700

    #2 Send prompts and receive images

--- src/pages/txt2Img.vue
+++ src/pages/txt2Img.vue
@@ -13,11 +13,13 @@ q-page
               //- @todo Add random placeholders
               q-input(v-model='prompt' label='Prompt' placeholder='a dr seuss illustration of robots building a city' autogrow @change='autosave')
                 template(v-slot:append='')
-                  q-btn(color='primary' label='Dream' icon='bubble_chart' :disabled='isStoppingDream' @click='queueDream')
+                  q-btn(color='primary' label='Dream' icon='bubble_chart' :disabled='isWakingUp' @click='queueDream')
                     //- q-tooltip(v-if='!settings.server || !settings.server.length' content-class='bg-red' content-style='font-size: 1em')
                     //-   | No servers selected
-                  q-btn(v-if='isDreaming || isStoppingDream' :disabled='isStoppingDream' :loading='isStoppingDream' color='negative' label='Stop' icon='cancel' @click='stopDreaming')
-              q-linear-progress.q-mt-md(size='20px' v-if='isDreaming || isStoppingDream' :value='dreamProgress' stripe='')
+                  q-btn(v-if='isDreaming || isWakingUp' :disabled='isWakingUp' :loading='isWakingUp' color='negative' label='Stop' icon='cancel' @click='stopDreaming')
+              template(v-for='server in settings.servers')
+                q-linear-progress.q-mt-md(size='20px' v-if='server.isDreaming || server.isWakingUp' :value='server.dreamProgress' stripe='')
+                  span(style='position: absolute; width: 100%; text-align: center; color: #fff; display: block; font-size: .65em') {{server.base}}
   .q-pa-md
     .q-col-gutter-md.row.items-start
       //- Config
@@ -55,13 +57,49 @@ import axios from 'axios'
 import {mapState} from 'vuex'
 import store from 'store'
 
-const autosaveFields = ['queue', 'tab', 'prompt', 'sessionHash', 'imgs', 'width', 'height', 'steps', 'batchSize']
+const autosaveFields = ['tab', 'prompt', 'sessionHash', 'imgs', 'width', 'height', 'steps', 'batchSize']
+
+/**
+ * Converts data into a format for specific Stable Diffusion apis
+ */
+const sdDataScribe = function (context) {
+  let data, promptDictionary, defaults
+
+  // Select statement based on data api
+  if (true) {
+    data = []
+    promptDictionary = ['prompt', 'negative', '', 'steps', '', '', '', 'numBatches', 'batchSize', '', '', '', '', '', '', 'height', 'width', '', '', '', '', '', '', '', '']
+    defaults = [context.defaultPrompt, '', 'None', 40, 'Euler a', false, false, context.numBatches, context.batchSize, 7, -1, -1, 0, 0, 0, context.height, context.width, 'None', null, 'Seed', '', 'Steps', '', false, []]
+    console.log('defaults', defaults)
+  
+    // Build the data fro the given dictionary and defaults
+    promptDictionary.forEach((key, n) => {
+      console.log(key)
+      if (key) {
+        data.push(context[key] || defaults[n])
+      } else {
+        data.push(defaults[n])
+      }
+    })
+  }
+
+  
+  return data
+}
 
 export default {
   name: 'IndexPage',
 
   computed: {
     ...mapState(['settings']),
+
+    isDreaming: function () {
+      return this.settings.servers.some(server => server.isDreaming)
+    },
+
+    isWakingUp: function () {
+      return this.settings.servers.some(server => server.isWakingUp)
+    },
   },
 
   mounted () {
@@ -83,6 +121,8 @@ export default {
     curDream: {},
     tab: 'Images',
     prompt: '',
+    negative: '',
+    
     // @todo add this to config
     defaultPrompt: 'a dr seuss illustration of robots building a city',
     // @todo generate and persist this (and do we even need this?)
@@ -93,10 +133,7 @@ export default {
     height: 512,
     steps: 40,
 
-    isDreaming: false,
-    isStoppingDream: false,
     dreamCheckInterval: 750, // milliseconds
-    dreamProgress: 0,
 
     numBatches: 1,
     batchSize: 1,
@@ -110,9 +147,15 @@ export default {
      * Queues up the dream and runs them if able to
      */
     queueDream () {
-      // @todo We shouldn't need these conditionals,
-      // instead set defaults when loading store
-      if (!this.queue) {this.queue = []}
+      // Exit if no servers
+      if (!this.settings.servers.length) {
+        this.$q.notify({
+          message: 'No servers selected. Set one in Settings',
+          position: 'top',
+          color: 'red'
+        })
+        return
+      }
 
       // Create the batch
       const batch = []
@@ -125,178 +168,111 @@ export default {
       this.queue.push(...batch)
 
       // Check and start the next dream
-      this.checkDream(this.startNextDream)
+      this.settings.servers.forEach(server => {
+        if (!server.isChecking) {
+          // Loop through each server and try to find an available one to run
+          const api = axios.create({ baseURL: server.base })
+
+          server.isChecking = true
+          this.checkDream(server, api)
+        }
+      })
     },
 
     /**
      * Check dream
-     * @param awakeCb Callback to call if there are no dreams
      */
-    checkDream (awakeCb) {
-      // Exit if no servers
-      if (!this.settings.servers.length) {
-        this.$q.notify({
-          message: 'No servers selected. Set one in Settings',
-          position: 'top',
-          color: 'red'
-        })
-        return
-      }
+    checkDream (server, api) {
+      console.log('checkDream', server)
       
-      // Loop through each server and try to find an available one to run
-      let hasFoundAvailableServer = false
-      for (let serverId = 0; serverId < this.settings.servers.length; serverId++) {
-        const api = axios.create({ baseURL: this.settings.servers[serverId].base })
+      api
+      .post('/api/predict', {
+        fn_index: 4,
+        data: [],
+        session_hash: this.sessionHash,
+      })
+      .then((response) => {
+        const data = response.data.data[0]
+        server.lastResponse = data
 
-        api
-        .post('/api/predict', {
-          fn_index: 4,
-          data: [],
-          session_hash: this.sessionHash,
-        })
-        .then((response) => {
-          const data = response.data.data[0]
+        // If we're dreaming, update the progress
+        // @todo handle multiple dreams
+        if (data.isGenerating) {
+          // Create a dummy DOM to extract the progress
+          const $dom = document.createElement('div')
+          $dom.innerHTML = data
+          const $width = $dom.querySelector('.progress')
 
-          // Checking to run
-          // If a server is available, start the dream. Otherwise wait a bit and check again
-          if (awakeCb) {
-            if (!data.isGenerating) {
-              !hasFoundAvailableServer && this.startNextDream(api, serverId)
-              hasFoundAvailableServer = true
-            } else {
-              if (!hasFoundAvailableServer) {
-                setTimeout(() => this.checkDream(awakeCb), this.dreamCheckInterval)
-              }
-            }
-
-          // Checking to get percent
+          // Update progress in UI
+          if ($width?.innerHTML) {
+            server.dreamProgress = parseInt($width.innerHTML.replace('%', '')) / 100
           } else {
-
-            // If we're dreaming, update the progress instead
-            // @todo handle multiple dreams
-            if (data.isGenerating) {
-  
-              // Create a dummy DOM to extract the progress
-              const $dom = document.createElement('div')
-              $dom.innerHTML = data
-              const $width = $dom.querySelector('.progress')
-  
-              // Update progress in UI
-              if ($width?.innerHTML) {
-                this.dreamProgress =
-                  parseInt($width.innerHTML.replace('%', '')) / 100
-              } else {
-                this.dreamProgress = 0
-              }
-  
-              // Check the dream again
-              if (this.isDreaming) {
-                setTimeout(() => {
-                  this.checkDream()
-                }, this.dreamCheckInterval)
-              }
-            }
+            server.dreamProgress = 0
           }
+
+          // Check the dream again
+          if (server.isDreaming) {
+            setTimeout(() => {
+              this.checkDream(server, api)
+            }, this.dreamCheckInterval)
+          }
+
+        // Otherwise start dreaming
+        } else {
+          !server.isDreaming && this.startDream(server, api)
+        }
+      })
+      .catch(err => {
+        this.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: `Error checking dream: ${err}`,
+          icon: 'report_problem',
         })
-        .catch(err => {
-          this.$q.notify({
-            color: 'negative',
-            position: 'top',
-            message: `Error checking dream: ${err}`,
-            icon: 'report_problem',
-          })
-        })
-      }
+      })
     },
 
     /**
      * Starts the dream and occasionally checks in to update progress
      */
-    startDream (api) {
-      this.isDreaming = true
+    startDream (server, api) {
+      server.isDreaming = true
+      server.progress = 0
+      
+      this.$nextTick(() => {
+        this.$forceUpdate()
 
-      setTimeout(() => {
-        this.checkDream()
-      }, 0)
+        setTimeout(() => {
+          this.checkDream(server, api)
+        }, 0)
+      })
 
-      // example: https://29390.gradio.app
       api
         .post('/api/predict', {
           fn_index: 3,
-          data: [
-            // @todo make this configurable
-            this.prompt || this.defaultPrompt,
-            // negative_prompt
-            '',
-            // txt2img_prompt_style
-            'None',
-            // init_img
-            this.steps,
-            // sampler_index
-            'Euler a',
-            // restore_faces
-            false,
-            // tiling
-            false,
-            // batch_count
-            this.numBatches,
-            // batch_size
-            this.batchSize,
-            // cfg_scale
-            7,
-            // seed
-            -1,
-            // subseed
-            -1,
-            // subseed_strength
-            0,
-            // seed_resize_from_h
-            0,
-            // seed_resize_from_w
-            0,
-            this.height,
-            this.width,
-
-            // Custom inputs
-            'None',
-            'Seed',
-            '',
-            'Steps',
-            '',
-            false,
-            [],
-            '',
-            '',
-          ],
+          data: sdDataScribe(this),
           session_hash: this.sessionHash,
         })
         .then((response) => {
-          // // Clean data
-          // const data = []
-          // response.data.data.forEach((val) => {
-          //   data.push(val)
-          // })
-          // console.log('RESPONSE', data, response)
+          // Clean data
+          const data = []
+          response.data.data.forEach((val) => {
+            data.push(val)
+          })
 
-          // const imgs = []
-          // data[0].forEach((img) => {
-          //   imgs.push(img)
-          // })
+          const imgs = []
+          data[0].forEach((img) => {
+            imgs.push(img)
+          })
 
-          // this.imgs.push(...imgs)
-          // this.status = data[1]
+          this.imgs.push(...imgs)
 
-          // // Run next in queue
-          // this.queue.unshift()
-          // if (this.queue.length) {
-          //   console.log('test', this.queue)
-          // }
-        })
-        .finally(() => {
-          this.isDreaming = false
-          this.isStoppingDream = false
+          // Run next in queue
+          server.isDreaming = false
+          server.isWakingUp = false
         })
         .catch((err) => {
+          console.log('CATCH', err)
           this.$q.notify({
             color: 'negative',
             position: 'top',
@@ -311,7 +287,7 @@ export default {
      *
      */
     stopDreaming () {
-      this.isStoppingDream = true
+      this.isWakingUp = true
 
       const api = axios.create({ baseURL: this.settings.servers[0].base })
       api
