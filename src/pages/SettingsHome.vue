3fb29c7824fcd2496ed3610cc495d062a5024cce
Everest Wonder <mounteverestwonder@gmail.com>
2022-09-15 11:34:01 -0700

    #27 Makes SettingsHome responsive

--- src/pages/SettingsHome.vue
+++ src/pages/SettingsHome.vue
@@ -7,12 +7,14 @@ q-page.full-height
       q-card-section
         .text-h6 GPU Servers
       q-card-section
-        p
-          | Use this to set the Stable Diffusion GPU server with a <code>api/predict</code> API, for example, like <a href="https://github.com/AUTOMATIC1111/stable-diffusion-webui">AUTOMATIC1111/stable-diffusion-webui</a> and <a href="https://github.com/sd-webui/stable-diffusion-webui">sd-webui/stable-diffusion-webui</a>. You can create a fast and free cloud server with Google Colab:
-          a(href='https://colab.research.google.com/drive/1Iy-xW9t1-OQWhb0hNxueGij8phCyluOh') https://colab.research.google.com/drive/1Iy-xW9t1-OQWhb0hNxueGij8phCyluOh
+        p Diffusion Blocks doesn't yet offer GPUs, you'll need to spin them up yourself. However this can be done for free! You can either use a local one through <a href="https://github.com/AUTOMATIC1111/stable-diffusion-webui">AUTOMATIC1111/stable-diffusion-webui</a> (v1.5) and <a href="https://github.com/sd-webui/stable-diffusion-webui">sd-webui/stable-diffusion-webui</a> (v1.5), or use a cloud hosted notebooks:
+        ul
+          li
+            | (v1.5) Google Colab:
+            a(href='https://colab.research.google.com/drive/1Iy-xW9t1-OQWhb0hNxueGij8phCyluOh') StableDiffusionUI-Voldemort.ipynb
 
       q-card-section
-        q-table(title='GPU Server List' :data='settings.servers' :columns='columns' row-key='base')
+        q-table(title='GPU Server List' :data='settings.servers' :columns='columns' row-key='base' :grid='$q.screen.lt.md')
           //- Add rows
           template(v-slot:top='')
             q-btn(color='secondary' icon='dns' label='Add GPU' @click='addRow')
@@ -20,15 +22,29 @@ q-page.full-height
           template(v-slot:body='props')
             q-tr(:props='props')
               //- Base
-              td(key='base' :props='props')
+              q-td(key='base' :props='props')
                 q-input(v-model='props.row.base' dense='' autofocus @keyup.enter='scope.set')
               //- API
-              td(key='api' :props='props')
+              q-td(key='api' :props='props')
                 q-select(v-model='props.row.api' :options='apiVersions' default='1.4' label='API')
               //- Actions
               q-td(key='actions' :props='props' v-if='settings.servers.length > 1')
                 q-toggle.q-mr-md(v-model='props.row.enabled' :label='props.row.enabled ? "Enabled" : "Disabled"' color='yellow')
                 q-btn(color='negative' icon='delete' @click='deleteRow(props)')
+          //- Responsive
+          template(v-slot:item='props')
+            .col-xs-12.col-sm-6.col-md-4
+              q-card
+                q-card-section
+                  p(key='base' :props='props')
+                    q-input(v-model='props.row.base' dense='' autofocus @keyup.enter='scope.set')
+                  //- API
+                  p(key='api' :props='props')
+                    q-select(v-model='props.row.api' :options='apiVersions' default='1.4' label='API')
+                  //- Actions
+                  p(key='actions' :props='props' v-if='settings.servers.length > 1')
+                    q-toggle.q-mr-md(v-model='props.row.enabled' :label='props.row.enabled ? "Enabled" : "Disabled"' color='yellow')
+                    q-btn(color='negative' icon='delete' @click='deleteRow(props)')
 </template>
 
 <script>
